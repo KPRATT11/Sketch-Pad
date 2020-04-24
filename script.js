1d2e160c461fe3576913fada0de740125c091470
KPRATT11 <61232612+KPRATT11@users.noreply.github.com>
2020-04-24 16:47:36 +1000

    V0.1.0 added drawing padd with opacity

---
+++ script.js
@@ -0,0 +1,45 @@
+const grid = document.querySelector('#grid');
+console.log(grid);
+
+let gridSize = 46;
+let divArray = [];
+let divArrayOppacity = [];
+
+//set up grid
+function setupGrid() {
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+for (let i =0; i < (gridSize * gridSize); i++){
+    divArray[i] = document.createElement('div')
+    divArray[i].classList.add('grid-cell')
+    divArray[i].style.padding = `${10 / gridSize}em`
+    divArray[i].style.backgroundColor = `rgba(0, 0, 0, 0)`   
+    grid.appendChild(divArray[i])
+    divArrayOppacity[i] = 0.1;
+}
+}
+
+setupGrid();
+let mouseDownToggle = false
+document.body.onmousedown = function(){
+    mouseDownToggle = true;
+}
+
+document.body.onmouseup = function () {
+    mouseDownToggle = false;
+}
+
+for (let i =0; divArray.length; i++){
+    divArray[i].addEventListener('mouseover', () => {
+        if (mouseDownToggle == true) {
+            divArray[i].style.backgroundColor = `rgba(0, 0, 0, ${divArrayOppacity[i]})`
+            divArrayOppacity[i] += 0.1;
+            console.log(divArray[i].style.opacity);  
+        }
+
+    })
+
+}
+
+
+
+    
