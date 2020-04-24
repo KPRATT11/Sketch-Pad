472b77b352129e18a72967e920cf0598349d219d
KPRATT11 <61232612+KPRATT11@users.noreply.github.com>
2020-04-25 09:36:46 +1000

    v 0.1.1 Added a reset button

--- script.js
+++ script.js
@@ -12,20 +12,38 @@ for (let i =0; i < (gridSize * gridSize); i++){
     divArray[i] = document.createElement('div')
     divArray[i].classList.add('grid-cell')
     divArray[i].style.padding = `${10 / gridSize}em`
-    divArray[i].style.backgroundColor = `rgba(0, 0, 0, 0)`   
+    divArray[i].style.backgroundColor = `rgba(0, 0, 0, 0)`  
+    divArray.draggable = false; 
     grid.appendChild(divArray[i])
     divArrayOppacity[i] = 0.1;
 }
 }
 
 setupGrid();
+
+function resetGrid() {
+    for (let i =0; i < divArray.length; i++){
+        divArray[i].style.backgroundColor = `rgba(0, 0, 0, 0)`;
+        divArrayOppacity[i] = 0.1;
+    }
+}
+
+//set drawing to activated when the mouse is down
 let mouseDownToggle = false
-document.body.onclick = function(){
-    mouseDownToggle = !mouseDownToggle;
+document.body.onmousedown = function(){
+    mouseDownToggle = true;
 }
 
 
-for (let i =0; divArray.length; i++){
+//deactivate drawing when the mouse is down
+document.body.onmouseup = function(){
+    mouseDownToggle = false;
+}
+
+
+
+//change pixels
+for (let i =0; i < divArray.length; i++){
     divArray[i].addEventListener('mouseover', () => {
         if (mouseDownToggle == true) {
             divArray[i].style.backgroundColor = `rgba(0, 0, 0, ${divArrayOppacity[i]})`
@@ -38,5 +56,10 @@ for (let i =0; divArray.length; i++){
 }
 
 
-
+//reset grid by calling setup grid
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', () => {
+    resetGrid();
+    console.log('yes');
+})
     
